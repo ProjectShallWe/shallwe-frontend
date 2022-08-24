d23b5456b4ed7591697710c5485f4240a5b413ab
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-24 14:38:29 +0900

    feat: Navigation Bar 구현

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <h1> 안녕하세요! </h1>
+  <Navbar />
+  <div class="container">
+    <router-view/>
+    <h1> 안녕하세요! </h1>
+  </div>
 </template>
 
 <script>
 
+import Navbar from "@/components/Navbar";
+
 export default {
   name: 'App',
   components: {
+    Navbar
   }
 }
 </script>
@@ -14,5 +21,30 @@ export default {
 <style>
 #app {
   font-family: 'Noto Sans KR', sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+button:focus,
+button:active,
+input:focus,
+input:active,
+textarea:focus,
+textarea:active {
+  outline: none;
+  box-shadow: none;
+}
+
+ul,
+ol,
+li {
+  list-style-type: none;
+  padding-left: 0;
+  margin-left: 0;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,96 @@
+<template>
+  <nav class="navigation-bar">
+    <div class="wrapper">
+      <!-- Logo -->
+      <router-link
+          :to="{name: 'home'}"
+      >
+        <h1>
+          ShallWe
+        </h1>
+      </router-link>
+      <!-- Global Link -->
+      <ul class="global-link">
+        <li>
+          <router-link
+              :to="{name: 'community'}"
+          >
+            커뮤니티
+          </router-link>
+        </li>
+        <li>
+          <router-link
+              :to="{name: 'community'}"
+          >
+            커뮤니티
+          </router-link>
+        </li>
+      </ul>
+      <!-- Login -->
+      <div class="login">
+        <router-link
+            cl
+            :to="{name: 'login'}"
+        >
+          로그인
+        </router-link>
+        <router-link
+            class=""
+            :to="{name: 'signUp'}"
+        >
+          회원가입
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  background-color: #D3D3D3;
+}
+
+.wrapper a h1 {
+  font-size: 16px;
+  padding: 16px;
+  margin-bottom: 0;
+}
+
+.global-link {
+  display: flex;
+  margin: auto 0;
+  font-size: 12px;
+}
+
+.global-link li::after {
+  content: "·";
+  margin-right: 3px;
+}
+
+.global-link li:last-child::after {
+  content: "";
+  margin-right: 0;
+}
+
+.login {
+  font-size: 12px;
+  padding: 16px;
+}
+
+.login a {
+  margin-right: 8px;
+}
+
+.login a:last-child {
+  margin-right: 0;
+}
+
+</style>
