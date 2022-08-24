53c2252f82b9d3fc0e1f7f681ad8f62e36e4159a
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-24 20:52:35 +0900

    feat: footer 스타일 구현

---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="contact">
+      <h2>
+        Contact
+      </h2>
+      <a href="https://github.com/cjsrhd94">GitHub</a>
+      <a href="https://cjsrhd94.tistory.com/">Blog</a>
+      <a href="mailto:cjsrhd94@gmail.com" title="문의 메일 보내기">Email</a>
+    </div>
+    <h3 class="copyright">Copyright © www.shallw2.com All rights reserved. </h3>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 20px;
+  background-color: #D3D3D3;
+}
+
+.contact {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.contact h2 {
+  font-size: 16px;
+  margin-bottom: 0px;
+  margin-right: 18px;
+}
+
+.contact a {
+  text-align: center;
+  font-size: 12px;
+  margin-right: 18px;
+}
+
+.copyright {
+  font-size: 12px;
+}
+</style>
