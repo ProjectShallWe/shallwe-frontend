025f9ee11451da39944d2b949b28cb3aa2e381f6
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-24 16:50:51 +0900

    feat: login page 스타일 구현

--- src/page/user/login.vue
+++ src/page/user/login.vue
@@ -1,12 +1,77 @@
 <template>
-  <h1> Login </h1>
+  <div class="row">
+    <div class="col-2">
+      <div class="wrapper">
+        <h1>로그인</h1>
+        <form action="" method="post">
+          <input type="email" placeholder="이메일 주소">
+          <input type="password" placeholder="비밀번호 (8자리 이상)">
+          <button type="submit">로그인</button>
+        </form>
+        <a href="">회원가입</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style scoped>
+.col-2 {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  border: #D3D3D3 solid 2px;
+  width: 100%;
+  min-width: 280px;
+  height: 420px;
+  padding: 20px;
+}
+
+.wrapper h1 {
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+
+.wrapper form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.wrapper input,
+.wrapper button {
+  height: 40px;
+  width: 100%;
+  border-radius: 4px;
+}
+
+.wrapper input {
+  font-size: 12px;
+  border: #D3D3D3 solid 1px;
+  color: #D3D3D3;
+  margin-bottom: 8px;
+}
+
+.wrapper button {
+  font-size: 16px;
+  color: #FFFFFF;
+  background-color: #8977AD;
+  border-style: none;
+  margin-bottom: 16px;
+}
+
+.wrapper button:hover {
+  opacity: 0.8;
+}
 </style>
