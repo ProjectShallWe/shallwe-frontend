08a2de541ff6d72e5cbf1e3ca6d56229b8212519
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-25 20:02:37 +0900

    refactor: PostListMini 컴포넌트 분리

---
+++ src/components/PostListMini.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="col-4">
+    <h2 class="board-title">게시판 이름</h2>
+    <ul class="board-recommend">
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+      <li class="recommend-post">
+        <a href="#">
+          <div>
+            게시글 카테고리
+          </div>
+          <div>
+            게시글 제목
+          </div>
+          <div>
+            100
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoardMini"
+}
+</script>
+
+<style scoped>
+.board-title {
+  text-align: center;
+  border-bottom: #8977AD solid 1px;
+  color: #000000;
+  font-size: 18px;
+  padding: 10px 8px;
+}
+
+.recommend-post a {
+  display: flex;
+  font-size: 14px;
+  padding: 10px 8px;
+}
+
+.recommend-post a div {
+  width: auto;
+  display: block;
+}
+</style>
--- src/page/community/board/_id.vue
+++ src/page/community/board/_id.vue
@@ -1,12 +1,31 @@
 <template>
-    <h1> Board Page </h1>
+  <div class="container">
+    <Header/>
+    <BoardCategory/>
+    <div class="row">
+      <BoardMini />
+      <BoardMini />
+      <BoardMini />
+    </div>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import BoardCategory from "@/components/BoardCategoryList";
+import BoardMini from "@/components/PostListMini";
+import Footer from "@/components/Footer";
+
 export default {
+  components: {
+    Header,
+    BoardCategory,
+    BoardMini,
+    Footer
+  },
 }
 </script>
 
 <style scoped>
-
 </style>
