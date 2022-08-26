1c178b98860e996bf2d1c3b4109d5bb0541f3949
cjsrhd94 <cjsrhd94@gmail.com>
2022-08-26 16:16:30 +0900

    feat: postcreate 컴포넌트 스타일 구현

---
+++ src/components/PostCreate.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="post-write">
+        <h1 class="post-write-title">
+          글쓰기
+        </h1>
+        <form action="" method="post">
+          <div class="post-title">
+            <select>
+              <option>카테고리 선택</option>
+              <option>축구1</option>
+              <option>축구2</option>
+              <option>축구3</option>
+            </select>
+            <input type="text" name="title">
+          </div>
+          <div class="post-content">
+            <textarea placeholder="글을 작성해주세요!"/>
+          </div>
+          <div class="button-group">
+            <button type="button" class="button-cancel">취소</button>
+            <button type="submit" class="button-submit">작성완료</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostCreate"
+}
+</script>
+
+<style scoped>
+.post-write-title {
+  font-size: 18px;
+  padding: 8px 0;
+}
+
+.post-title {
+  display: flex;
+  padding: 8px 0;
+}
+
+.post-title select {
+  margin-right: 8px;
+}
+
+.post-title input {
+  width: 100%;
+  border: #D3D3D3 solid 1px;
+}
+
+.post-content {
+  padding: 8px 0;
+}
+.post-content textarea {
+  width: 100%;
+  height: 500px;
+  border: #D3D3D3 solid 1px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 0;
+}
+
+.button-group button {
+  border-radius: 8px;
+  padding: 8px;
+}
+
+.button-group .button-submit {
+  color: #FFFFFF;
+  background-color: #8977AD;
+}
+</style>
--- src/page/community/board/create/index.vue
+++ src/page/community/board/create/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1> PostCreate </h1>
+  <div class="container">
+    <Header />
+    <BoardCategoryList></BoardCategoryList>
+    <PostCreate />
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import BoardCategoryList from "@/components/BoardCategoryList";
+import PostCreate from "@/components/PostCreate";
 export default {
+  components: {
+    PostCreate,
+    BoardCategoryList,
+    Header,
+    Footer
+  }
 }
 </script>
 
