985058d94766c49aa7271ebc8c1e149b5b8d66db
cjsrhd94 <cjsrhd94@gmail.com>
2022-10-31 23:32:56 +0900

    feat: landing page scss 수정

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,15 +1,15 @@
 <template>
   <section class="landing">
     <h1 class="landing-title">
-      <strong>
+      <strong class="logo">
         ShallWe
       </strong>
     </h1>
     <h2 class="landing-subtitle">
       '우리 같이 놀아요'
     </h2>
-    <p class="landing-desc">
-      <strong>
+    <p class="landing-desc fs-m">
+      <strong class="logo">
         ShallWe
       </strong>
       는 누구나 참여할 수 있는 커뮤니티를 만드는 것을 목표로 하고 있습니다.
@@ -32,27 +32,19 @@ export default {
   align-items: center;
   text-align: center;
   height: 100vh;
-  color: #FFFFFF;
-  background-color: #8977AD;
+  color: $PRIMARY_COLOR;
+  background-color: $SECONDARY_COLOR;
+
+  .landing-title {
+    display: block;
+    font-size: 96px;
+    margin-bottom: 12px;
+  }
+
+  .landing-subtitle {
+    font-size: 32px;
+    margin-bottom: 48px;
+  }
 }
 
-.landing-title {
-  display: block;
-  font-size: 96px;
-  font-style: italic;
-  margin-bottom: 12px;
-}
-
-.landing-subtitle {
-  font-size: 32px;
-  margin-bottom: 48px;
-}
-
-.landing-desc {
-  font-size: 16px;
-}
-
-.landing-desc strong {
-  font-style: italic;
-}
 </style>
