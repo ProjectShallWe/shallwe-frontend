e72fc4003d485ed29f3c617da9133ba4fe4556a3
cjsrhd94 <cjsrhd94@gmail.com>
2022-11-01 22:51:53 +0900

    feat: PostDetail 컴포넌트 scss 적용 및 스타일 변경

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -82,113 +82,113 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .post-category {
-  font-size: 16px;
-  padding: 8px 0;
-  border-bottom: #D3D3D3 solid 1px;
+  font-size: 14px;
+  padding: 8px 16px;
+  border-bottom: $TERTIARY_COLOR solid 1px;
 }
 
 .post-title {
   display: flex;
   justify-content: space-between;
   align-content: center;
-  padding: 12px 0;
-  border-bottom: #D3D3D3 solid 1px;
+  padding: 12px 16px;
+  border-bottom: $TERTIARY_COLOR solid 1px;
   width: 100%;
-}
 
-.post-title h1 {
-  font-size: 24px;
-}
+  h1 {
+    font-size: 24px;
+    max-width: 85%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 
-.post-title span {
-  font-size: 14px;
-  line-height: 29px;
-  /*display: block;*/
-}
-
-.post-info {
-  padding: 8px 0;
-  border-bottom: #D3D3D3 solid 1px;
-}
-
-.post-writer {
-  font-size: 14px;
-}
-
-.post-hit {
-  display: block;
-  align-content: center;
-  font-size: 14px;
-  line-height: 20px;
-}
-
-.post-hit span {
-  font-size: 13px;
-  padding: 0 6px;
+  span {
+    font-size: 14px;
+    line-height: 29px;
+  }
 }
 
 .post-info {
   display: flex;
   justify-content: space-between;
+  padding: 8px 16px;
+  border-bottom: $TERTIARY_COLOR solid 1px;
+
+  .post-writer {
+    font-size: 14px;
+  }
+
+  .post-hit {
+    display: block;
+    align-content: center;
+    font-size: 14px;
+    line-height: 20px;
+
+    span {
+      font-size: 13px;
+      padding: 0 6px;
+    }
+  }
 }
 
 .post-content {
-  padding: 16px 0;
-}
+  padding: 16px;
 
-.post-content a {
-  color: #1a0dab;
-}
+  a {
+    color: #1a0dab;
+  }
 
-.post-content blockquote {
-  padding-left: 1rem;
-  border-left: 2px solid rgba(13, 13, 13, 0.1);
+  blockquote {
+    padding-left: 1rem;
+    border-left: 2px solid rgba(13, 13, 13, 0.1);
+  }
 }
 
 .post-reaction {
   display: flex;
   justify-content: center;
-}
 
-.post-like-button {
-  display: flex;
-  border: #D3D3D3 solid 1px;
-  border-radius: 4px;
-  padding: 12px 20px;
-  cursor: pointer;
-}
+  .post-like-button {
+    display: flex;
+    border: $TERTIARY_COLOR solid 1px;
+    border-radius: 4px;
+    padding: 12px 20px;
+    cursor: pointer;
 
-.post-like-count {
-  font-size: 16px;
-  color: #C34747;
-}
+    .post-like-count {
+      font-size: 16px;
+      color: $EMPHASIS_COLOR;
 
-.post-like-count::before {
-  display: inline-block;
-  content: "";
-  background-image: url("../assets/images/thumb_up_black_24dp.svg");
-  background-repeat: no-repeat;
-  background-size: 100%;
-  width: 24px;
-  height: 21px;
-  margin-right: 4px;
+      &:before {
+        display: inline-block;
+        content: "";
+        background-image: url("../assets/images/thumb_up_black_24dp.svg");
+        background-repeat: no-repeat;
+        background-size: 100%;
+        width: 24px;
+        height: 21px;
+        margin-right: 4px;
+      }
+    }
+  }
 }
 
 .comment-list-header {
   display: flex;
-  padding: 8px 0;
-  border-bottom: #D3D3D3 solid 1px;
-}
+  padding: 8px;
+  border-bottom: $TERTIARY_COLOR solid 1px;
 
-.comment-list-header h3,
-.comment-list-header span {
-  font-size: 15px;
-}
+  h3 {
+    font-size: 15px;
+    margin-top: 2px;
+  }
 
-.comment-list-header h3 {
-  margin-top: 2px;
-}
+  span {
+    font-size: 15px;
+  }
 
+}
 </style>
