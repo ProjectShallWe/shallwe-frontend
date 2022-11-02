540a81806216f907d31ee3dd50e2d837c8955154
cjsrhd94 <cjsrhd94@gmail.com>
2022-11-03 01:00:32 +0900

    feat: CommentList 컴포넌트 scss 적용 및 스타일 변경

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -2,6 +2,7 @@
   <div class="row">
     <div class="col-12">
       <ul class="comment-list">
+        <!-- 댓글 작성 양식 -->
         <div v-if="loggedIn"
              class="reply-write-form"
         >
@@ -10,47 +11,56 @@
               @submit.prevent="writeParentComment"
               class="reply-write-content"
           >
-            <textarea
-                v-model="parentContent"
-                placeholder="댓글을 작성해주세요."
-            />
-            <button type="submit">등록</button>
+            <div class="reply-content">
+              <textarea
+                  v-model="parentContent"
+                  placeholder="댓글을 작성해주세요."
+              />
+            </div>
+            <div class="reply-bottom">
+              <button type="submit">등록</button>
+            </div>
           </form>
         </div>
-        <li v-if="isEmpty()"
-             class="empty"
+        <!-- post 내 작성된 댓글이 없을 때 -->
+        <li v-if="isEmpty(commentList)"
+            class="empty"
         >
           작성된 댓글이 없습니다. 첫 댓글을 남겨주세요.
         </li>
+        <!-- 댓글 보여주기 -->
         <li v-for="comment in commentList"
             :key="comment.commentId"
             class="comment">
-          <div class="comment-info">
-            <div>
-              <span class="comment-writer">{{ comment.nickname }}</span>
-              <span class="comment-created-date">({{ comment.createdDate }})</span>
-            </div>
-            <div class="comment-reaction">
-              <div @click = "addLikeCount(comment.commentId)"
-                  class="comment-like-button">
-                <span class="comment-like-count">
-                  {{comment.likeCommentCount}}
-                </span>
+          <div class="comment-wrapper">
+            <div class="comment-info">
+              <div>
+                <span class="comment-writer">{{ comment.nickname }}</span>
+                <span class="comment-created-date">({{ comment.createdDate }})</span>
+              </div>
+              <div class="comment-reaction">
+                <div @click="addLikeCount(comment.commentId)"
+                     class="comment-like-button">
+                  <span class="comment-like-count">
+                    {{ comment.likeCommentCount }}
+                  </span>
+                </div>
               </div>
             </div>
+            <div class="comment-content">
+              <p v-html="getContent(comment.content)"></p>
+            </div>
+            <div class="reply-line">
+              <button
+                  v-if="loggedIn"
+                  @click="showReplyWriteForm(comment)"
+                  type="button"
+              >
+                답글
+              </button>
+            </div>
           </div>
-          <div class="comment-content">
-            <p v-html="getContent(comment.content)"></p>
-          </div>
-          <div class="reply-line">
-            <button
-                v-if="loggedIn"
-                @click="showReplyWriteForm(comment)"
-                type="button"
-            >
-              답글
-            </button>
-          </div>
+          <!-- 답글 작성 양식 -->
           <div
               v-if="comment.isShowReplyWriteForm && loggedIn"
               class="reply-write-form"
@@ -60,37 +70,45 @@
                 @submit.prevent="writeChildComment(comment.commentId)"
                 class="reply-write-content"
             >
-              <textarea
-                  v-model="childContent"
-                  placeholder="답글을 작성해주세요."
-              />
-              <button type="submit">등록</button>
+              <div class="reply-content">
+                <textarea
+                    v-model="childContent"
+                    placeholder="답글을 작성해주세요."
+                />
+              </div>
+              <div class="reply-bottom">
+                <button type="submit">등록</button>
+              </div>
             </form>
           </div>
-          <ul class="reply-list">
+          <!-- 대댓글 보여주기 -->
+          <ul v-if="!isEmpty(comment.childComments)"
+              class="reply-list">
             <li
                 v-for="reply in comment.childComments"
                 :key="reply.commentId"
                 class="reply"
             >
-              <div class="reply-info">
-                <div>
-                  <span class="reply-writer">{{ reply.nickname }}</span>
-                  <span class="reply-created-date">({{ reply.createdDate }})</span>
-                </div>
-                <div class="comment-reaction">
-                  <div @click = "addLikeCount(reply.commentId)"
-                       class="comment-like-button">
+              <div class="reply-wrapper">
+                <div class="comment-info">
+                  <div>
+                    <span class="comment-writer">{{ reply.nickname }}</span>
+                    <span class="comment-created-date">({{ reply.createdDate }})</span>
+                  </div>
+                  <div class="comment-reaction">
+                    <div @click="addLikeCount(reply.commentId)"
+                         class="comment-like-button">
                     <span class="comment-like-count">
-                      {{reply.likeCommentCount}}
+                      {{ reply.likeCommentCount }}
                     </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="reply-content">
-                <p v-html="getContent(reply.content)"></p>
-              </div>
-              <div class="reply-line">
+                <div class="comment-content">
+                  <p v-html="getContent(reply.content)"></p>
+                </div>
+                <div class="reply-line">
+                </div>
               </div>
             </li>
           </ul>
@@ -115,8 +133,8 @@ export default {
 
     const loggedIn = computed(() => store.getters["login/loggedIn"]);
 
-    const isEmpty = () => {
-      const arr = commentList.value;
+    const isEmpty = (list) => {
+      const arr = list;
       return Array.isArray(arr) && arr.length === 0;
     }
 
@@ -194,124 +212,136 @@ export default {
 }
 </script>
 
-<style scoped>
-.comment {
-  border-bottom: #D3D3D3 solid 1px;
-}
+<style lang="scss" scoped>
 
-.comment-list .empty {
-  text-align: center;
-  padding: 24px 0;
-  border-top: #D3D3D3 solid 1px;
-  border-bottom: #D3D3D3 solid 1px;
-}
+.comment-list {
+  margin-bottom: 24px;
 
-.comment-info,
-.reply-info {
-  display: flex;
-  justify-content: space-between;
-  padding: 8px 0;
-}
+  .reply-write-form {
+    background-color: #F8F8F8;
 
-.comment-writer,
-.reply-writer {
-  font-size: 14px;
-  padding: 0 8px;
-}
+    h3 {
+      display: block;
+      font-size: 14px;
+      padding: 16px 16px 0 16px;
+    }
 
-.comment-created-date,
-.reply-created-date {
-  font-size: 12px;
-}
+    .reply-write-content {
+      align-items: center;
+      padding: 16px;
 
-.comment-content,
-.reply-content {
-  font-size: 14px;
-  padding: 8px;
-}
+      .reply-content {
+        textarea {
+          width: 100%;
+          height: 100px;
+          font-size: 14px;
+          margin-bottom: 8px;
+          padding: 8px;
+        }
+      }
 
-.comment-content p,
-.reply-content p {
-  width: 100%;
-}
+      .reply-bottom {
+        display: flex;
+        justify-content: right;
 
-.comment-reaction {
-  display: flex;
-  align-content: center;
-}
+        button {
+          font-size: 13px;
+          height: 25px;
+          width: 40px;
+          color: $PRIMARY_COLOR;
+          background-color: $SECONDARY_COLOR;
+          border-radius: 8px;
+        }
+      }
+    }
+  }
 
-.comment-like-button {
-  display: flex;
-  border: #C34747 solid 1px;
-  border-radius: 8px;
-  padding: 2px;
-  cursor: pointer;
-}
+  .empty {
+    text-align: center;
+    padding: 24px 0;
+    border-top: #D3D3D3 solid 1px;
+    border-bottom: #D3D3D3 solid 1px;
+  }
 
-.comment-like-count {
-  font-size: 12px;
-}
+  .comment {
+    border-bottom: #D3D3D3 solid 1px;
 
-.comment-like-count::before {
-  display: inline-block;
-  content: "";
-  background-image: url("../assets/images/thumb_up_black_24dp.svg");
-  background-repeat: no-repeat;
-  background-size: 100%;
-  width: 14px;
-  height: 12px;
-  margin-right: 4px;
-}
+    .comment-wrapper {
+      padding: 16px
+    }
 
-.reply {
-  border-top: #D3D3D3 solid 1px;
-  background-color: #F8F8F8;
-}
+    .reply-list {
+      padding: 16px 16px 16px 36px;
+      background-color: #F8F8F8;
+    }
+  }
 
-.reply-list {
-  margin-left: 16px;
-}
+  .comment-info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 8px;
 
-.reply-line {
-  display: flex;
-  justify-content: right;
-  font-size: 11px;
-  height: 28px;
-}
+    .comment-writer {
+      font-size: 14px;
+      margin-right: 8px;
+    }
 
-.reply-line button {
-  color: #D3D3D3;
-  background-color: #FFFFFF;
-}
+    .comment-created-date {
+      font-size: 12px;
+    }
 
-.reply-write-form {
-  background-color: #F8F8F8;
-}
+    .comment-reaction {
+      display: flex;
+      align-content: center;
 
-.reply-write-content {
-  display: flex;
-  align-items: center;
-  padding: 16px;
-}
+      .comment-like-button {
+        display: flex;
+        border: #C34747 solid 1px;
+        border-radius: 8px;
+        padding: 2px;
+        cursor: pointer;
 
-.reply-write-form h3 {
-  display: block;
-  font-size: 13px;
-  padding: 16px 16px 0 16px;
-}
+        .comment-like-count {
+          font-size: 12px;
 
-.reply-write-content textarea {
-  width: 100%;
-  height: 60px;
-  margin-right: 16px;
-}
+          &:before {
+            display: inline-block;
+            content: "";
+            background-image: url("../assets/images/thumb_up_black_24dp.svg");
+            background-repeat: no-repeat;
+            background-size: 100%;
+            width: 14px;
+            height: 12px;
+            margin-right: 4px;
+          }
+        }
+      }
+    }
+  }
 
-.reply-write-content button {
-  height: 60px;
-  width: 60px;
-  color: #FFFFFF;
-  background-color: #8977AD;
-  border-radius: 8px;
+  .comment-content {
+    font-size: 14px;
+
+    p {
+      width: 100%;
+    }
+  }
+
+  .reply-list {
+    padding-left: 36px;
+    background-color: #F8F8F8;
+  }
+
+  .reply-line {
+    display: flex;
+    justify-content: right;
+    font-size: 11px;
+
+    button {
+      color: #D3D3D3;
+      background-color: #FFFFFF;
+    }
+  }
 }
 
 </style>
